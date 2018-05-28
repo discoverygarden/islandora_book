dc2f9d076fe5e0232da1c37834f0a9bab6cf0e39
willtp87 <willtp87@hotmail.com>
2018-05-28 07:59:03 -0300

    dmu

--- theme/islandora-book-page.tpl.php
+++ theme/islandora-book-page.tpl.php
@@ -5,7 +5,10 @@
  */
 ?>
 <?php
-  print $book_object_id ? l(t('Return to Book View'), "islandora/object/{$book_object_id}") : t('Orphaned page (no associated book)');
+  // @FIXME
+// l() expects a Url object, created from a route name or external URI.
+// print $book_object_id ? l(t('Return to Book View'), "islandora/object/{$book_object_id}") : t('Orphaned page (no associated book)');
+
 ?>
 <?php if (isset($viewer)): ?>
   <div id="book-page-viewer">
@@ -14,11 +17,23 @@
 <?php elseif (isset($object['JPG']) && islandora_datastream_access(ISLANDORA_VIEW_OBJECTS, $object['JPG'])): ?>
   <div id="book-page-image">
     <?php
-      $params = array(
-        'path' => url("islandora/object/{$object->id}/datastream/JPG/view"),
-        'attributes' => array(),
-      );
-      print theme('image', $params);
+      // @FIXME
+// url() expects a route name or an external URI.
+// $params = array(
+//         'path' => url("islandora/object/{$object->id}/datastream/JPG/view"),
+//         'attributes' => array(),
+//       );
+
+      // @FIXME
+// theme() has been renamed to _theme() and should NEVER be called directly.
+// Calling _theme() directly can alter the expected output and potentially
+// introduce security issues (see https://www.drupal.org/node/2195739). You
+// should use renderable arrays instead.
+// 
+// 
+// @see https://www.drupal.org/node/2195739
+// print theme('image', $params);
+
     ?>
   </div>
 <?php endif; ?>
