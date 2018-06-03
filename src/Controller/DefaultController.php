65f1eb12d47e99d754a24403cefaf5b8618f156d
willtp87 <willtp87@hotmail.com>
2018-06-03 20:42:59 -0300

    wiring pages view

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Drupal\islandora_book\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Access\AccessResult;
+
+/**
+ * Default controller for the islandora_book module.
+ */
+class DefaultController extends ControllerBase {
+
+  /**
+   * Pages display generator.
+   */
+  public function pages($object = NULL) {
+    return islandora_book_pages_menu($object);
+  }
+
+  /**
+   * Access callback function for display of pages.
+   */
+  public function pagesAccess($object = NULL) {
+    $object = islandora_object_load($object);
+    $permissions = [ISLANDORA_VIEW_OBJECTS];
+    $content_models = ['islandora:bookCModel'];
+    $perm = islandora_user_access_check($object, $permissions, $content_models);
+    return $perm ? AccessResult::allowed() : AccessResult::forbidden();
+  }
+
+}
